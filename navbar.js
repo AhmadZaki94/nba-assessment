8c5c491998d257d82cabd6a2f58153656bd8705d
Ahmad Zaki <shaikhzaki8@gmail.com>
2022-10-18 13:20:25 +0530

    update

---
+++ navbar.js
@@ -0,0 +1,12 @@
+function navbar() {
+  return `<div id="container">
+    <p class="pageName">
+        <a class="route" href="index.html">Players Page</a>
+    </p>
+    <p class="pageName">
+        <a class="route" href="games.html">Games Page</a>
+    </p>
+</div>`;
+}
+
+export default navbar;
